9ad2ec732cacb68d009985d645f94459df0d74d5
Nikhil Manu <70922592+NikhilManu@users.noreply.github.com>
2024-09-23 16:31:44 +0530

    RP-203: Random valid

--- config.py
+++ config.py
@@ -3,7 +3,7 @@ FIFTEEN_5DAYS = '( {cash} ( latest sma( latest volume , 20 ) * latest close >= 3
 FIFTY_40DAYS = '( {cash} ( latest sma( latest volume , 20 ) * latest close >= 30000000 and latest close >= 50 days ago close * 1.5 ) )'
 TEN_DECLINE_5DAYS = '( {cash} ( latest sma( latest volume , 20 ) * latest close >= 30000000 and latest close <= 5 days ago close * 0.90 ) )'
 
-
+#s S
 # Setups
 IPOBASES = '( {cash} ( ( {cash} not( 12 months ago close > 0 ) ) and latest "close - 1 candle ago close / 1 candle ago close * 100" < 1 and latest "close - 1 candle ago close / 1 candle ago close * 100" > -1 and latest volume * latest close >= 10000000 and market cap > 100 ) )'
 MAGNITUDE_SETUPS = '( {cash} ( 1 week ago close <= 2 weeks ago high and 1 week ago open <= 2 weeks ago high and 1 week ago close >= 2 weeks ago low and 1 week ago open >= 2 weeks ago low and latest close >= 50 and weekly close <= 1 week ago high and weekly open <= 1 week ago high and weekly close >= 1 week ago low and weekly open >= 1 week ago low and latest sma( latest volume , 20 ) * latest close >= 50000000 and weekly close <= 2 weeks ago high and weekly open <= 2 weeks ago high and weekly open <= 2 weeks ago high and weekly open >= 2 weeks ago low ) )'
