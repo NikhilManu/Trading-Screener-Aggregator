47de39febe7570a7c6eb9eacadf5c84e62907f4b
Nikhil Manu <70922592+NikhilManu@users.noreply.github.com>
2025-11-12 18:57:06 +0530

    Added and updated some scan configurations

--- config.py
+++ config.py
@@ -1,23 +1,31 @@
 # Weekend Scans
-ONE_MONTH = "( {cash} ( latest volume * latest close >= 100000000 and latest close >= 21 days ago close * 1.30 and( {166311} not( latest close > 0 ) ) ) )"
+ONE_MONTH = '( {cash} ( daily {custom_indicator_190145_start}"volume *  close"{custom_indicator_190145_end} >= 200000000 and latest close >= 21 days ago close * 1.30 and( {166311} not( latest close > 0 ) ) ) )'
 
 # Daily Scan
-FIFTEEN_5DAYS = "( {cash} ( latest volume * latest close >= 100000000 and latest close >= 5 days ago close * 1.15 and( {166311} not( latest close > 0 ) ) ) )"
-SIX_1DAY = "( {cash} ( daily close >= 1 day ago close * 1.06 and daily volume > daily sma( daily volume , 50 ) * 3 and latest volume * daily close >= 100000000 and( {166311} not( daily close > 0 ) ) ) )"
-GAP_UP = "( {cash} ( daily open > 1 day ago close * 1.04 and latest volume * daily close >= 100000000 and( {166311} not( daily close > 0 ) ) ) )"
+FIVE_5DAYS = "( {cash} ( market cap >= 70000 and latest close >= 5 days ago close * 1.05 and( {166311} not( latest close > 0 ) ) ) )"
+FIFTEEN_5DAYS = '( {cash} ( daily {custom_indicator_190145_start}"volume *  close"{custom_indicator_190145_end} >= 200000000 and latest close >= 5 days ago close * 1.15 and( {166311} not( latest close > 0 ) ) ) )'
+SIX_1DAY = '( {cash} ( daily {custom_indicator_190145_start}"volume *  close"{custom_indicator_190145_end} >= 200000000 and daily close >= 1 day ago close * 1.06 and daily volume > 1 day ago volume and( {166311} not( daily close > 0 ) ) ) )'
+GAP_UP = '( {cash} ( daily {custom_indicator_190145_start}"volume *  close"{custom_indicator_190145_end} >= 200000000 and daily open > 1 day ago close * 1.04 and( {166311} not( daily close > 0 ) ) ) )'
 
 # IPO Setups
 IPOBASES = '( {cash} ( ( {cash} not( 12 months ago close > 0 ) ) and latest "close - 1 candle ago close / 1 candle ago close * 100" <= 1 and latest "close - 1 candle ago close / 1 candle ago close * 100" => -1 and market cap > 5000 ) )'
 IPO_IB = '( {cash} ( ( {cash} not( 12 months ago close > 0 ) ) and daily high <= 1 day ago high and daily low >= 1 day ago low and market cap > 5000 ) )'
 
-# Momentum Setups
-T_ATR = '( {cash} ( ( {cash} ( latest volume * latest close >= 100000000 and daily sma( close,7 ) / daily sma( close,65 ) >= 1.05 and daily high - daily low <= daily avg true range( 14 ) / 2 and( {166311} not( daily close > 0 ) ) ) ) ) )'
-DIB_SETUP = '( {cash} ( ( {cash} ( daily volume * daily close >= 100000000 and daily sma( close,7 ) / daily sma( close,65 ) >= 1.05 and daily high < 1 day ago high and daily low > 1 day ago low and 1 day ago high < 2 days ago high and 1 day ago low > 2 days ago low ) ) and( {166311} not( daily close > 0 ) ) ) )'
-TT_SETUP = '( {cash} ( ( {cash} ( daily volume * daily close >= 100000000 and daily "close - 1 candle ago close / 1 candle ago close * 100" <= .5 and 1 day ago "close - 1 candle ago close / 1 candle ago close * 100" <= 1 and daily "close - 1 candle ago close / 1 candle ago close * 100" >= -.5 and 1 day ago "close - 1 candle ago close / 1 candle ago close * 100" >= -1 and daily sma( close,7 ) / daily sma( close,65 ) >= 1.05 and( {166311} not( daily close > 0 ) ) ) ) ) )'
+# MB
+DIB_TI65 = '( {cash} ( daily {custom_indicator_190145_start}"volume *  close"{custom_indicator_190145_end} >= 200000000 and daily {custom_indicator_190070_start}"sma(  close , 7 ) /  sma(  close , 65 )"{custom_indicator_190070_end} >= 1.05 and daily high < 1 day ago high and daily low > 1 day ago low and 1 day ago high < 2 days ago high and 1 day ago low > 2 days ago low and( {166311} not( daily close > 0 ) ) ) )'
+DIB_MOM = '( {cash} ( daily {custom_indicator_190145_start}"volume *  close"{custom_indicator_190145_end} >= 200000000 and daily {custom_indicator_190151_start}"close /  min( 30 ,  close )"{custom_indicator_190151_end} >= 1.20 and daily high < 1 day ago high and daily low > 1 day ago low and 1 day ago high < 2 days ago high and 1 day ago low > 2 days ago low and( {166311} not( daily close > 0 ) ) ) )'
+T_TI65 = '( {cash} ( daily {custom_indicator_190145_start}"volume *  close"{custom_indicator_190145_end} >= 200000000 and daily {custom_indicator_190070_start}"sma(  close , 7 ) /  sma(  close , 65 )"{custom_indicator_190070_end} >= 1.05 and daily "close - 1 candle ago close / 1 candle ago close * 100" <= 0.4 and daily "close - 1 candle ago close / 1 candle ago close * 100" >= -0.4 and( {166311} not( daily close > 0 ) ) ) )'
+T_MOM = '( {cash} ( daily {custom_indicator_190145_start}"volume *  close"{custom_indicator_190145_end} >= 200000000 and daily {custom_indicator_190151_start}"close /  min( 30 ,  close )"{custom_indicator_190151_end} >= 1.20 and daily "close - 1 candle ago close / 1 candle ago close * 100" <= 0.4 and daily "close - 1 candle ago close / 1 candle ago close * 100" >= -0.4 and( {166311} not( daily close > 0 ) ) ) )'
+
+# FHP
+FHP_TTT = '( {cash} ( market cap >= 70000 and daily "close - 1 candle ago close / 1 candle ago close * 100" <= 0.5 and daily "close - 1 candle ago close / 1 candle ago close * 100" >= -0.5 and 1 day ago "close - 1 candle ago close / 1 candle ago close * 100" <= 1 and 1 day ago "close - 1 candle ago close / 1 candle ago close * 100" >= -1 and 2 days ago "close - 1 candle ago close / 1 candle ago close * 100" <= 1 and 2 days ago "close - 1 candle ago close / 1 candle ago close * 100" >= -1 and( {166311} not( daily close > 0 ) ) ) )'
+FHP_DIB = '( {cash} ( market cap >= 70000 and daily high < 2 days ago high and daily low > 2 days ago low and 1 day ago high < 2 days ago high and 1 day ago low > 2 days ago low and( {166311} not( daily close > 0 ) ) ) )'
 
 clauses = {
     "DAILY SCAN": [FIFTEEN_5DAYS, SIX_1DAY, GAP_UP],
     "WEEKEND SCAN": [ONE_MONTH],
     "IPO": [IPOBASES, IPO_IB],
-    "MB": [T_ATR, DIB_SETUP, TT_SETUP],
+    "MB": [DIB_MOM, DIB_TI65, T_MOM, T_TI65],
+    "FHP SETUP": [FHP_TTT, FHP_DIB],
+    "FHP STUDY": [FIVE_5DAYS]
 }
